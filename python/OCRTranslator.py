b4d8b3b15d03fce64060677f23d12a9c8ea89089
tae6ae <tae6ae@gmail.com>
2021-10-19 15:04:03 +0900

    Modify OCRTranslator.py

--- python/OCRTranslator.py
+++ python/OCRTranslator.py
@@ -4,39 +4,46 @@ from PIL import Image
 import os
 import matplotlib.pyplot as plt
 from googletrans import Translator
+import sys
 
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
+def OCRTranslate(fileName, sLang, dLang) :
+    pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
 
-sourceLang = "eng"
-destLang = "ko"
-inputFileName = "Restaurant"
+    sourceLang = sLang
+    destLang = dLang
+    inputFileName = fileName
 
-imageFileName = "image\{}.jpg".format(inputFileName)
-image = cv2.imread(imageFileName, cv2.IMREAD_COLOR)
+    print(sourceLang, destLang, inputFileName)
 
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-gray = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
-#kernel_size = 5
-#gray = cv2.medianBlur(gray, kernel_size)
+    imageFileName = "image\{}.jpg".format(inputFileName)
+    image = cv2.imread(imageFileName, cv2.IMREAD_COLOR)
 
-tempFileName = "temp\{}.png".format(inputFileName)
-cv2.imwrite(tempFileName, gray)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    gray = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+    #kernel_size = 5
+    #gray = cv2.medianBlur(gray, kernel_size)
 
-text = pytesseract.image_to_string(Image.open(tempFileName), lang=sourceLang)
-os.remove(tempFileName)
+    tempFileName = "temp\{}.jpg".format(inputFileName)
+    cv2.imwrite(tempFileName, gray)
 
-textFileName = "result\{}2.txt".format(inputFileName)
-with open(textFileName, 'w', encoding='utf-8') as f :
-    f.write(text)
+    text = pytesseract.image_to_string(Image.open(tempFileName), lang=sourceLang)
+    os.remove(tempFileName)
 
-#cv2.imshow("Image", image)
-#cv2.imshow("Gray", gray)
-#cv2.waitKey(0)
+    #textFileName = "result\{}2.txt".format(inputFileName)
+    #with open(textFileName, 'w', encoding='utf-8') as f :
+    #    f.write(text)
 
-translator = Translator()
+    #cv2.imshow("Image", image)
+    #cv2.imshow("Gray", gray)
+    #cv2.waitKey(0)
 
-result = translator.translate(text, dest=destLang)
+    translator = Translator()
 
-resultFileName = "result\{}.txt".format(inputFileName)
-with open(resultFileName, 'w', encoding='utf-8') as f :
-    f.write(result.text)
+    result = translator.translate(text, dest=destLang)
+
+    resultFileName = "result\{}.txt".format(inputFileName)
+    with open(resultFileName, 'w', encoding='utf-8') as f :
+        f.write(result.text)
+
+if __name__ == '__main__' :
+    OCRTranslate(sys.argv[1], sys.argv[2], sys.argv[3])
